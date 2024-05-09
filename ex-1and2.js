f7033a96a1c4465aec33692ba1fbf3d2d19fb21c
Thanasak <ma.thanasak@gmail.com>
2024-05-09 12:32:07 +0700

    feat: Variable Practice Exam 1-4

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,10 @@
 //Exercise #1: Variable Creation
 //Start coding here
-
+let firstName = "Karter";
+let lastName = "Anderson";
+let address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai";
 //Exercise #2: Variable Reassignment
 //Start coding here
+const nameRestaurant = "Chef's Table by Chef James";
+const totalBill = 15000;
+const OpenRestaurant = 1500;
