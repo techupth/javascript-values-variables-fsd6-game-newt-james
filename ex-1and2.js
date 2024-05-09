1aae480f5b7f929e301028fb73b8b03b91272ff2
tanayod <tanayod741@gmail.com>
2024-05-09 13:40:36 +0700

    feat:ex-1and2

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,15 @@
 //Exercise #1: Variable Creation
-//Start coding here
+let firstName = "Karter";
+let lastName = "Anderson";
+let Address = "54/24 Gp Bangkruay-Sainoi Bang Kruay Bang Kruai";
+
+const restaurantName = "Chef's Table by Chef James";
+const foodPrice = 15000;
+const numberOfRestaurant = 1500;
 
 //Exercise #2: Variable Reassignment
 //Start coding here
+
+Address = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641";
+foodPrice = 5000;
+numberOfRestaurant = 2500;
